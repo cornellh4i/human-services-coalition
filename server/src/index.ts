f82148a7f1f0e238af6c4143ee8337fbcaa37e20
wg237 <miyukigoay@gmail.com>
2022-08-28 17:05:48 -0400

    Conversion from js to ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 const appName = "Server API"; 
 const port = process.env.PORT || 8080;
-const createServer = require("./server");
-const server = createServer();
+const serverInit = require("./server");
+const server = serverInit();
 server.listen(port, () => console.log(`${appName} running on port ${port}!`));
