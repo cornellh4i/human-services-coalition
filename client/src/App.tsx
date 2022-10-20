30b7dfb22548f4684042134e376dc8e55945060b
Louis Valencia <lcv28@cornell.edu>
2022-10-20 18:58:24 -0400

    Added pages files and Edited App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
-import {useEffect, useState} from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import Home from "./pages/Home"
 const API_URL = process.env.REACT_APP_API;
+
 
 function App() {
   const [data, setData] = useState("No data :(");
-  
+
   useEffect(() => {
     async function getData() {
       const url = `${API_URL}/hello`;
@@ -12,13 +15,21 @@ function App() {
       setData(data.msg);
     }
     getData();
-  }, []); 
+  }, []);
 
   return (
-    <>
-      <h1>MERN App!</h1>
-      <p>Data from server: {data}</p>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Go Taylor Swift</h2>
+    </div>
+  )
+}
+
+export default Home
