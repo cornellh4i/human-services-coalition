0cc0caca3ac6f9eacc3eeb4c0d1996f430a0e8f1
Kaden Lei <kl646@cornell.edu>
2022-11-08 16:22:12 -0500

    kinda started the filter

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from 'react'
 import ListingDetails from '../components/ListingDetails'
 import ListingForm from "../forms/ListingForm"
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import { borders } from '@mui/system'
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles'
+import { Typography } from '@mui/material'
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 
 const Home = () => {
   const [Listings, setListings] = useState<any[]>([])
@@ -21,12 +34,42 @@ const Home = () => {
   return (
     <div className="home">
       <div className="listings">
+        <Box sx={{
+          flexGrow: 1,
+          position: "absolute",
+          width: '23%',
+          height: 869,
+          left: 16,
+          top: 103,
+          background: "#FFFFFF",
+          boxShadow: "0px 4px 4px rgba(93, 115, 126, 0.5)",
+          borderRadius: 10,
+          p: 2,
+          pl: 3,
+          pt: 1
+        }}>
+          <Grid container spacing={2} columns={12}>
+            <Grid item xs={12}>
+            </Grid>
+            <Grid item xs={12}>
+              <ThemeProvider theme={theme}>
+                <Typography variant="h4" sx={{
+                  fontFamily: 'Poppins',
+                  fontStyle: "normal",
+                  fontWeight: 700,
+                  fontSize: 24,
+                  textAlign: "left",
+                }}>Filters</Typography>
+              </ThemeProvider>
+            </Grid>
+          </Grid>
+        </Box>
         {Listings && Listings.map((Listing) => (
           <ListingDetails key={Listing._id} Listing={Listing} />
         ))}
         <ListingForm />
       </div>
-    </div>
+    </div >
   )
 }
 
