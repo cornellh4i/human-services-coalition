b7c0536153e4d9a9b487b5f5f1274f79994ff1c9
Eric Ellis Freitag <eef49@cornell.edu>
2023-02-15 17:55:05 -0500

    Causes the navbar to shrink dynamically

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -7,12 +7,14 @@ import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { ReactComponent as Logo } from '../assets/coclogo.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import '../css/NavBar.css';
 
 const pages = ['Create New', 'Manage Profiles', 'Update FMR'];
-const settings = ['Listing', 'User', 'Admin'];
+const forms = ['Listing', 'User', 'Admin'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -30,6 +32,7 @@ function ResponsiveAppBar() {
   };
 
   const handleCloseUserMenu = () => {
+    handleCloseNavMenu();
     setAnchorElUser(null);
   };
 
@@ -37,6 +40,7 @@ function ResponsiveAppBar() {
   const navigate = useNavigate();
 
   const navigateToProfiles = () => {
+    handleCloseNavMenu();
     navigate("/manage-profiles");
   };
 
@@ -44,8 +48,52 @@ function ResponsiveAppBar() {
     <AppBar position="sticky" elevation={0} style={{ background: "white", borderBottom: '1px solid #D9D9D9' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box textAlign={'left'}
-            sx={{ p: 0 }}>
+          <Box textAlign={'left'} sx={{ p: 0, display: { xs: 'none', md: 'flex' }}}>
+            <Button
+              component={Link} to="/"
+              sx={{ width: 300, p: 0, border: 0, justifyContent: "flex-start" }}>
+              <Logo height={75} href="" />
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="primary"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={page === "Create New" ? handleOpenUserMenu : page === "Manage Profiles" ? navigateToProfiles : handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box sx={{ p: 0, display: { xs: 'flex', md: 'none' }}}>
             <Button
               component={Link} to="/"
               sx={{ width: 300, p: 0, border: 0, justifyContent: "flex-start" }}>
@@ -100,14 +148,14 @@ function ResponsiveAppBar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
+              {forms.map((form) => (
                 <MenuItem 
-                  key={setting} 
+                  key={form} 
                   onClick={handleCloseUserMenu} 
                   component={Link} 
-                  to={setting === "Listing" ? 'listing-form' : setting === "Admin" ? 'admin-form' : setting === 'User' ? 'user-form' : ''}
+                  to={form === "Listing" ? 'listing-form' : form === "Admin" ? 'admin-form' : form === 'User' ? 'user-form' : ''}
                 >
-                  <Typography className='text' textAlign="left" margin='5px'>{setting}</Typography>
+                  <Typography className='text' textAlign="left" margin='5px'>{form}</Typography>
                 </MenuItem>
               ))}
             </Menu>
